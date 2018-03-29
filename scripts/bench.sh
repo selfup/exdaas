41b245a664de0696d1ce9b87b41b36f5ff4ac7cb
selfup <boudinot.regis@yahoo.com>
2018-03-29 09:13:08 -0500

    update bench script

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,4 +1,18 @@
-logfile=.results.log
+LOG_FILE=.results.log
+
+function cflag () {
+  if [ "$1" == "-c" ]
+  then
+    echo "--> entire benchmark output:
+      updated in $(pwd)/$logfile
+    "
+  else
+    git checkout -- $logfile
+  fi
+}
+
+# if you pass the -c flag
+# the script will keep changes in git for the logfile
 
 ab \
   -n 50000 \
@@ -6,21 +20,10 @@ ab \
   -k -v 1 \
   -H "Accept-Encoding: gzip, deflate" \
   -T "application/json" \
-  -p ./scripts/bench.data.json http://0.0.0.0:4000/api > $logfile \
+  -p ./scripts/bench.data.json http://0.0.0.0:4000/api > $LOG_FILE \
   && echo "" \
   && echo "--> results:
-    $(grep seconds $logfile)
-    $(grep -w second $logfile)
-  "
-
-# if you pass the -c flag
-# the script will keep changes in git for the logfile
-
-if [ "$1" == "-c" ]
-then
-  echo "--> entire benchmark output:
-    updated in $(pwd)/$logfile
-  "
-else
-  git checkout -- $logfile
-fi
+    $(grep seconds $LOG_FILE)
+    $(grep -w second $LOG_FILE)
+  " \
+  && cflag $1 \
