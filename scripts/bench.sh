4983d986bafc78866ddb64d2b22bd2f2da9e4434
selfup <boudinot.regis@yahoo.com>
2018-03-28 23:52:27 -0500

    update bench script

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,7 +1,23 @@
+logfile=.results.log
+
 ab \
   -n 5000 \
   -c 100 \
   -k -v 1 \
   -H "Accept-Encoding: gzip, deflate" \
   -T "application/json" \
-  -p ./scripts/bench.data.json http://0.0.0.0:4000/api > .results.log
+  -p ./scripts/bench.data.json http://0.0.0.0:4000/api > $logfile \
+  && echo "" \
+  && echo "--> results:
+    $(grep seconds $logfile)
+    $(grep -w second $logfile)
+  "
+
+if [ $BENCH_LOG ]
+then
+  echo "--> entire benchmark output:
+    updated in $(pwd)/$logfile
+  "
+else
+  git checkout -- $logfile
+fi
