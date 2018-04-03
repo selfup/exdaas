78990e6cd1ff4a61b369d0a2554d69f5d287d68a
selfup <boudinot.regis@yahoo.com>
2018-04-03 09:33:18 -0500

    make docker rm all everything forever :wave:

---
+++ scripts/docker.stop.rm.sh
@@ -0,0 +1,14 @@
+PS_AQ=$(docker ps -aq)
+IMAGES_Q=$(docker images -q)
+
+if [ !$PS_AQ ] || [ !$IMAGES_Q ]
+then
+    echo 'NOTHING TO STOP OR DELETE'
+else
+    echo ' STOPPING ALL CONTAINERS' \
+        && docker stop $PS_AQ \
+        && echo 'DELETING ALL CONTAINERS' \
+        && docker rm $PS_AQ \
+        && echo 'DELETING ALL IMAGES' \
+        && docker rmi $IMAGES_Q
+fi
